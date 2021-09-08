9bf4119ba14d63447f1857a1cb4dfb27652227a2
tratchapong <tratchapong@gmail.com>
2021-09-08 07:08:06 +0700

    Simple with no partials

---
+++ models/todoModel.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const {v4:uuid} = require('uuid')
+const {readFileJSON, writeFileJSON} = require("../helper")
+
+const dbs = 'dbs/todo.json'
+
+class todoModel {
+  constructor(list, status) {
+    this.list = list
+    this.status= status
+  }
+
+  static findAll = async () => {
+    let data = await readFileJSON(dbs)
+    return data
+  }
+
+  static findOne = async (id) => {
+    let data = await readFileJSON(dbs)
+    return data.find(x => x.id === id)
+  }
+
+  static remove = async (id) => {
+    let data = await readFileJSON(dbs)
+    let del_id = data.findIndex(x=> x.id === id)
+    let delItem = data.splice(del_id,1)
+    await writeFileJSON(dbs, data)
+    return delItem
+  }
+
+  static add = async (list) => {
+    let data = await readFileJSON(dbs)
+    let newTodo = {id: uuid(), list, status: false}
+    data.push(newTodo)
+    await writeFileJSON(dbs, data)
+    return newTodo
+  }
+  
+  static replace = async (item) => {
+    let data = await readFileJSON(dbs)
+    let r_id = data.findIndex(x=> item.id === x.id)
+    data[r_id] = {...item}
+    await writeFileJSON(dbs, data)
+    return data[r_id]
+  }
+}
+
+module.exports = todoModel
---
+++ routers/todoRouter.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const todoRouter = express.Router()
+const todoController = require('../controllers/todoController')
+
+todoRouter.get('/', todoController.getAll )
+todoRouter.get('/addForm', todoController.addForm)
+todoRouter.get('/edit/:id', todoController.editForm)
+todoRouter.post('/saveEdit', todoController.saveEdit)
+todoRouter.get('/delete/:id', todoController.delete )
+todoRouter.get('/:id', todoController.getIndex )
+todoRouter.delete('/:id', todoController.delete)
+todoRouter.post('/', todoController.add)
+
+module.exports = todoRouter
